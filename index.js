679240d7d21cea6659320b43a96f5f4e7314184e
Nadun Jayamaha <nadunjayamaha45@gmail.com>
2023-08-26 11:12:41 +0530

    Router Changes

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 import express from "express";
 import connectDB from "./db.js";
-import cors from "cors";
 
-import { getUser, createUser } from "./controllers/userController.js";
-import {
-  getDailyProgress,
-  getWorkouts,
-  getMonthlyProgress,
-} from "./controllers/workoutController.js";
+import {getUser,createUser} from "./controllers/userController.js"
+import { getDailyProgress,getWorkouts,getMonthlyProgress } from "./controllers/workoutController.js";
 
-const app = express();
-app.listen(5000, () => console.log("server started at 5000"));
-app.use(express.json());
 
-app.use(cors());
 
-app.use("/api/user", userRoutes);
-app.use("/api/workouts", workoutRoutes);
-app.use("/api/getDailyProgress", fitbitRoutes);
 
+const app = express()
+app.listen(5000,() => console.log('server started at 5000'))
+app.use(express.json())
+
+app.get('/api/user',getUser)
+app.post('/api/user',createUser)
+app.get('/api/workouts',getWorkouts)
+app.get('/api/getDailyProgress/:date',getDailyProgress)
+app.get('/api/getMonthlyProgress/:startDate/:endDate',getMonthlyProgress)
+
+ 
 connectDB();
