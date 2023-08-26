e2e5598c0d3ac38bc6a6b69750bdf881f0119489
pasan uvindu <pasanuvindu7@gmail.com>
2023-08-26 10:49:03 +0530

    Install cors

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 import express from "express";
 import connectDB from "./db.js";
+import cors from "cors";
 
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
 import workoutRoutes from "./routes/workoutRoutes.js";
 import fitbitRoutes from "./routes/fitBitRoutes.js";
 
+const app = express();
+app.listen(5000, () => console.log("server started at 5000"));
+app.use(express.json());
 
+app.use(cors());
 
+app.use("/api/user", userRoutes);
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/getDailyProgress", fitbitRoutes);
 
-const app = express()
-app.listen(5000,() => console.log('server started at 5000'))
-app.use(express.json())
-
-app.use('/api/user',userRoutes)
-app.use('/api/workouts',workoutRoutes)
-app.use('/api/getDailyProgress',fitbitRoutes)
-
- 
 connectDB();
