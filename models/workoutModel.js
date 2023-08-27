8d0412c400e69fe4dded792f371d3bef960065ef
Nadun Jayamaha <nadunjayamaha45@gmail.com>
2023-08-27 10:55:05 +0530

    Workout filter added

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose";
 
 const workoutSchema = mongoose.Schema({
-    workoutName : String,
-    workoutType : String,
-    sets : Number,
-    reps : Number
+    workoutName: String,
+    workoutType: String,
+    sets: Number,
+    reps: Number,
+    caloriesEstimation: Number,
+    image: String,
+    duration: Number,
+    intensity: String,
+    steps: [String],
+    workoutPlace: String,
 })
 
 const Workout = mongoose.model('Workouts',workoutSchema,'Workouts')
