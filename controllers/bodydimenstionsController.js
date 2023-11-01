8c7284121755a162bce79b6abf1fe1644a7f13f3
Nadun Jayamaha <nadunjayamaha45@gmail.com>
2023-11-01 20:21:18 +0530

    Detection Handling

--- controllers/bodydimenstionsController.js
+++ controllers/bodydimenstionsController.js
@@ -3,30 +3,33 @@ import Dimensions from '../models/bodydimenstionsModel.js'
 
 
 export const getDimensions = async (req, res) => {
-    try {
-      const { name } = req.params;
-  
-      if (!name) {
-        return res.status(400).json({ error: 'Name parameter is required' });
-      }
-  
-      const dimensions = await Dimensions.findOne({ imageName: name });
-      console.log("Dimnesions :" + dimensions)
-  
-      if (!dimensions) {
-        return res.status(404).json({ error: 'Dimensions not found' });
-      }
-  
-      
-      const hipWasiRatio = parseFloat(dimensions.ratio);
-      const gender = dimensions.gender
-      
-      
-      console.log("GENDER "+ gender)
-      
+  try {
+    const { name } = req.params;
 
-      let riskLevel;
-  
+    if (!name) {
+      return res.status(400).json({ error: 'Name parameter is required' });
+    }
+
+    const dimensions = await Dimensions.findOne({ imageName: name });
+    console.log("Dimnesions :" + dimensions)
+
+    if (!dimensions) {
+      return res.status(404).json({ error: 'Dimensions not found' });
+    }
+
+    let riskLevel;
+    let hipWasiRatio
+
+    if (dimensions.ratio == "Not Detected") {
+       hipWasiRatio = 'Not Detected';
+       riskLevel = 'Not Detected';
+    }
+    else {
+
+      hipWasiRatio = parseFloat(dimensions.ratio);
+      const gender = dimensions.gender
+      console.log("GENDER " + gender)
+
       if (dimensions.gender === 'female') {
         if (hipWasiRatio <= 0.80) {
           riskLevel = 'low';
@@ -44,13 +47,15 @@ export const getDimensions = async (req, res) => {
           riskLevel = 'high';
         }
       }
-  
-      console.log('Hip Waist Ratio:', hipWasiRatio);
-      console.log('Risk Level:', riskLevel);
-  
-      res.status(200).json({ dimensions, hipWasiRatio, riskLevel });
-    } catch (error) {
-      res.status(500).json({ error: 'An error occurred' });
+
     }
-  };
-  
+
+    console.log('Hip Waist Ratio:', hipWasiRatio);
+    console.log('Risk Level:', riskLevel);
+
+    res.status(200).json({ dimensions, hipWasiRatio, riskLevel });
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred' });
+  }
+};
+
